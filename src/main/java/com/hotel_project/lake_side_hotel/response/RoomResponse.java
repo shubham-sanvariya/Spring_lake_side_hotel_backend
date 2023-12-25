b91230c48a5d14f78fb0a045e1917ab31bcecb38
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2023-12-25 20:57:50 +0530

    adding Room Response

---
+++ src/main/java/com/hotel_project/lake_side_hotel/response/RoomResponse.java
@@ -0,0 +1,37 @@
+package com.hotel_project.lake_side_hotel.response;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class RoomResponse {
+    private Long Id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+    private String photo;
+    private List<BookingResponse> bookings;
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        Id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType,
+                        BigDecimal roomPrice,
+                        boolean isBooked,
+                        byte[] photoBytes,
+                        List<BookingResponse> bookings) {
+        Id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
+        this.bookings = bookings;
+    }
+}
