44fab280099d2c275a97c79009a1d700b675d070
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2023-12-26 13:46:20 +0530

    adding Room Service and it's impl

---
+++ src/main/java/com/hotel_project/lake_side_hotel/service/RoomServiceImpl.java
@@ -0,0 +1,31 @@
+package com.hotel_project.lake_side_hotel.service;
+
+import com.hotel_project.lake_side_hotel.model.Room;
+import com.hotel_project.lake_side_hotel.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImpl implements IRoomService{
+    private final RoomRepository roomRepository;
+    @Override
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice) throws IOException, SQLException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if (!file.isEmpty()){
+            byte[] photoBytes = file.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+        return roomRepository.save(room);
+    }
+}
