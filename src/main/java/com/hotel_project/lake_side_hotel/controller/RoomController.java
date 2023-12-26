753e0dc649d0a35cc3d983be55e639aea7c0e2ae
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2023-12-26 13:47:05 +0530

    adding RoomController with req to add new room

---
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/RoomController.java
@@ -0,0 +1,32 @@
+package com.hotel_project.lake_side_hotel.controller;
+
+import com.hotel_project.lake_side_hotel.model.Room;
+import com.hotel_project.lake_side_hotel.response.RoomResponse;
+import com.hotel_project.lake_side_hotel.service.IRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rooms")
+public class RoomController {
+
+    private final IRoomService roomService;
+
+    @PostMapping("/add/new-room")
+    public ResponseEntity<RoomResponse> addNewRoom(
+            @RequestParam("photo") MultipartFile photo,
+            @RequestParam("roomType") String roomType,
+            @RequestParam("roomPrice") BigDecimal roomPrice) throws SQLException, IOException {
+        Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
+        RoomResponse response = new RoomResponse(savedRoom.getId(),
+                savedRoom.getRoomType(), savedRoom.getRoomPrice());
+        return ResponseEntity.ok(response);
+    }
+}
