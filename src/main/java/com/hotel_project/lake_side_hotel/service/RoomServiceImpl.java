eaf83d9e26662633b229523eb704b10b1efb7abf
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2023-12-31 12:59:22 +0530

    adding getAllRoomTypes in RoomServiceImpl

--- src/main/java/com/hotel_project/lake_side_hotel/service/RoomServiceImpl.java
+++ src/main/java/com/hotel_project/lake_side_hotel/service/RoomServiceImpl.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +28,10 @@ public class RoomServiceImpl implements IRoomService{
             room.setPhoto(photoBlob);
         }
         return roomRepository.save(room);
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return roomRepository.findDistinctRoomTypes();
     }
 }
