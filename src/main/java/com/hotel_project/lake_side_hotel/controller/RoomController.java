76c8b42f7396ff390cc8c73fd9c436b0a9eb6cfd
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-02 14:00:30 +0530

    adding mapping to get all-rooms & delete roomById

--- src/main/java/com/hotel_project/lake_side_hotel/controller/RoomController.java
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/RoomController.java
@@ -1,16 +1,25 @@
 package com.hotel_project.lake_side_hotel.controller;
 
+import com.hotel_project.lake_side_hotel.exception.PhotoRetrievalException;
+import com.hotel_project.lake_side_hotel.model.BookedRoom;
 import com.hotel_project.lake_side_hotel.model.Room;
+import com.hotel_project.lake_side_hotel.response.BookingResponse;
 import com.hotel_project.lake_side_hotel.response.RoomResponse;
+import com.hotel_project.lake_side_hotel.service.BookingService;
 import com.hotel_project.lake_side_hotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +27,7 @@ import java.sql.SQLException;
 public class RoomController {
 
     private final IRoomService roomService;
+    private final BookingService bookingService;
 
     @PostMapping("/add/new-room")
     public ResponseEntity<RoomResponse> addNewRoom(
@@ -28,5 +38,61 @@ public class RoomController {
         RoomResponse response = new RoomResponse(savedRoom.getId(),
                 savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes() {
+        return roomService.getAllRoomTypes();
+    }
+
+    @GetMapping("/all-rooms")
+    public ResponseEntity<List<RoomResponse>> getAllRooms() throws SQLException {
+        List<Room> rooms = roomService.getAllRooms();
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for (Room room : rooms) {
+            byte[] photoByte = roomService.getRoomPhotoByRoomId(room.getId());
+            if (photoByte != null && photoByte.length > 0){
+                String base64Photo = Base64.encodeBase64String(photoByte);
+                RoomResponse roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(base64Photo);
+                roomResponses.add(roomResponse);
+            }
+        }
+        return ResponseEntity.ok(roomResponses);
+    }
+
+    private RoomResponse getRoomResponse(Room room) {
+        List<BookedRoom> bookings = getAllBookingsByRoomId(room.getId());
+//        List<BookingResponse> bookingInfo = bookings
+//                .stream()
+//                .map(booking -> new BookingResponse(booking.getBookingId(),
+//                        booking.getCheckInDate(),
+//                        booking.getCheckOutDate(),
+//                        booking.getBookingConfirmationCode())).toList();
+
+        byte[] photoBytes = null;
+        Blob photoBlob = room.getPhoto();
+        if (photoBlob != null){
+            try{
+                photoBytes = photoBlob.getBytes(1,(int) photoBlob.length());
+            }catch (SQLException e){
+                throw new PhotoRetrievalException("Error retrieving photo");
+            }
+        }
+        return new RoomResponse(room.getId(),
+                room.getRoomType(),
+                room.getRoomPrice(),
+                room.isBooked(),
+                photoBytes);
+    }
+
+    private List<BookedRoom> getAllBookingsByRoomId(Long roomID) {
+        return bookingService.getAllBookingsByRoomId(roomID);
+    }
+
+    @DeleteMapping("/delete/room/{roomId}")
+    public ResponseEntity<Void> deleteRoom(@PathVariable("roomId") Long roomId){
+        roomService.deleteRoom(roomId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/hotel_project/lake_side_hotel/response/RoomResponse.java
+++ src/main/java/com/hotel_project/lake_side_hotel/response/RoomResponse.java
@@ -14,7 +14,7 @@ public class RoomResponse {
     private BigDecimal roomPrice;
     private boolean isBooked;
     private String photo;
-    private List<BookingResponse> bookings;
+//    private List<BookingResponse> bookings;
 
     public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
         this.id = id;
@@ -25,13 +25,12 @@ public class RoomResponse {
     public RoomResponse(Long id, String roomType,
                         BigDecimal roomPrice,
                         boolean isBooked,
-                        byte[] photoBytes,
-                        List<BookingResponse> bookings) {
+                        byte[] photoBytes) {
         this.id = id;
         this.roomType = roomType;
         this.roomPrice = roomPrice;
         this.isBooked = isBooked;
         this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
-        this.bookings = bookings;
+//        this.bookings = bookings;
     }
 }
