a255648ae2346d6fa0283e390267f54dbb254e74
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-02 19:54:28 +0530

    adding InternalServerException

---
+++ src/main/java/com/hotel_project/lake_side_hotel/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.hotel_project.lake_side_hotel.exception;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
