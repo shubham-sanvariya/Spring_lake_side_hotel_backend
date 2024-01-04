b8b76f33f4027b39a47fddb7531501ade483daf6
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-04 20:32:09 +0530

    adding BookedRoomController with get allbookings mapping

---
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/BookedRoomController.java
@@ -0,0 +1,34 @@
+package com.hotel_project.lake_side_hotel.controller;
+
+import com.hotel_project.lake_side_hotel.exception.ResourceNotFoundException;
+import com.hotel_project.lake_side_hotel.model.BookedRoom;
+import com.hotel_project.lake_side_hotel.response.BookingResponse;
+import com.hotel_project.lake_side_hotel.service.IBookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/bookings")
+public class BookedRoomController {
+    private final IBookingService bookingService;
+
+    @GetMapping("/all-bookings")
+    public ResponseEntity<List<BookingResponse>> getAllBookings(){
+        List<BookedRoom> bookings = bookingService.getAllBookings();
+        List<BookingResponse> bookingResponses = new ArrayList<>();
+        for (BookedRoom booking : bookings){
+            BookingResponse bookingResponse = getBookingResponse(booking);
+            bookingResponses.add(bookingResponse);
+        }
+        return ResponseEntity.ok(bookingResponses);
+    }
+}
