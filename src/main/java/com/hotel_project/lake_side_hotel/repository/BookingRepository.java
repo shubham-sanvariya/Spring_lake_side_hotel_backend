3bdffa65e6140dadda7441baafb2ff723d644fba
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-09 21:54:24 +0530

    making findByConfirmationCode optional

--- src/main/java/com/hotel_project/lake_side_hotel/repository/BookingRepository.java
+++ src/main/java/com/hotel_project/lake_side_hotel/repository/BookingRepository.java
@@ -4,10 +4,11 @@ import com.hotel_project.lake_side_hotel.model.BookedRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<BookedRoom, Long> {
 
     List<BookedRoom> findByRoomId(Long roomId);
 
-    BookedRoom findByBookingConfirmationCode(String confirmationCode);
+    Optional<BookedRoom> findByBookingConfirmationCode(String confirmationCode);
 }
