387922359cc5a28bbd1f356e431ea3eaa5d46784
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-12 19:44:02 +0530

    adding RoleRepository with findByName method

---
+++ src/main/java/com/hotel_project/lake_side_hotel/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.hotel_project.lake_side_hotel.repository;
+
+import com.hotel_project.lake_side_hotel.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String role);
+}
