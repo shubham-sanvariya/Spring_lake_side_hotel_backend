6d5b905a307a94a7c8c94b723041d12a2ae77614
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-12 11:44:36 +0530

    adding Role model

---
+++ src/main/java/com/hotel_project/lake_side_hotel/model/Role.java
@@ -0,0 +1,25 @@
+package com.hotel_project.lake_side_hotel.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Collection<User> users = new HashSet<>();
+}
