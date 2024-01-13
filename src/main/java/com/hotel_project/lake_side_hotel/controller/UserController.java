ae0cd700d8e6fddf1f2df57eeb2580761b22246f
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-13 12:08:47 +0530

    adding UserController with method getAllUsers

---
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.hotel_project.lake_side_hotel.controller;
+
+import com.hotel_project.lake_side_hotel.model.User;
+import com.hotel_project.lake_side_hotel.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
+    }
+}
