e22a6c895b16c2d82bbc89a9a65cc04c1d8b7676
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-13 12:59:09 +0530

    adding Delete mapping for deleteUser

--- src/main/java/com/hotel_project/lake_side_hotel/controller/UserController.java
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/UserController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,20 @@ public class UserController {
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error fetching user");
+        }
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
+        try {
+            userService.deleteUser(email);
+            return ResponseEntity.ok("User deleted successfully");
+        }catch (UsernameNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error deleting user");
         }
     }
 }
