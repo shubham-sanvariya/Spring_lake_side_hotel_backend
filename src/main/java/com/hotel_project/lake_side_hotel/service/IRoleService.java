575d33f55067007ef780679929c5d2891d4b35f1
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-14 12:52:06 +0530

    adding createRole method in IRoleService & it's impl

--- src/main/java/com/hotel_project/lake_side_hotel/service/IRoleService.java
+++ src/main/java/com/hotel_project/lake_side_hotel/service/IRoleService.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface IRoleService {
 
     List<Role> getRoles();
+    Role createRole(Role theRole);
+
 }
--- src/main/java/com/hotel_project/lake_side_hotel/service/RoleServiceImpl.java
+++ src/main/java/com/hotel_project/lake_side_hotel/service/RoleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hotel_project.lake_side_hotel.service;
 
+import com.hotel_project.lake_side_hotel.exception.RoleAlreadyExistsException;
 import com.hotel_project.lake_side_hotel.model.Role;
 import com.hotel_project.lake_side_hotel.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +14,15 @@ public class RoleServiceImpl implements IRoleService{
     @Override
     public List<Role> getRoles() {
         return roleRepository.findAll();
+    }
+
+    @Override
+    public Role createRole(Role theRole) {
+        String roleName = "ROLE_" + theRole.getName().toUpperCase();
+        Role role = new Role(roleName);
+        if (roleRepository.existsByName(role)){
+            throw new RoleAlreadyExistsException(theRole.getName()+ " role already exists");
+        }
+        return roleRepository.save(role);
     }
 }
