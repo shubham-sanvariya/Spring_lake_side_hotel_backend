54a009d84ccd4a7a7eabe9487d2935666d3bb5a5
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-17 11:29:18 +0530

    adding authenticateUsers method

--- src/main/java/com/hotel_project/lake_side_hotel/controller/AuthController.java
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/AuthController.java
@@ -2,24 +2,63 @@ package com.hotel_project.lake_side_hotel.controller;
 
 import com.hotel_project.lake_side_hotel.exception.UserAlreadyExistsException;
 import com.hotel_project.lake_side_hotel.model.User;
+import com.hotel_project.lake_side_hotel.request.LoginRequest;
+import com.hotel_project.lake_side_hotel.response.JwtResponse;
+import com.hotel_project.lake_side_hotel.security.jwt.JwtUtils;
+import com.hotel_project.lake_side_hotel.security.user.HotelUserDetails;
 import com.hotel_project.lake_side_hotel.service.IUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final IUserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
     @PostMapping("/register-user")
-    public ResponseEntity<?> registerUser(User user){
+    public ResponseEntity<?> registerUser(@RequestBody User user){
         try {
+            System.out.println(user.getEmail());
             userService.registerUser(user);
             return ResponseEntity.ok("User Registered successfully");
         }catch (UserAlreadyExistsException e){
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .body(e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUsers(@Valid @RequestBody
+                                               LoginRequest request){
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        request.getEmail(), request.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtTokenForUser(authentication);
+        HotelUserDetails userDetails = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return ResponseEntity.ok(new JwtResponse(
+                userDetails.getId(),
+                userDetails.getEmail(),
+                jwt,
+                roles
+        ));
     }
 }
