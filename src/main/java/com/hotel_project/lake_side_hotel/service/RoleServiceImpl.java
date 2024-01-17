0ec4d2a01c10653917969da03311dd7ded740c34
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-17 11:03:07 +0530

    correcting typo role to roleName

--- src/main/java/com/hotel_project/lake_side_hotel/service/RoleServiceImpl.java
+++ src/main/java/com/hotel_project/lake_side_hotel/service/RoleServiceImpl.java
@@ -28,7 +28,7 @@ public class RoleServiceImpl implements IRoleService{
     public Role createRole(Role theRole) {
         String roleName = "ROLE_" + theRole.getName().toUpperCase();
         Role role = new Role(roleName);
-        if (roleRepository.existsByName(role)){
+        if (roleRepository.existsByName(roleName)){
             throw new RoleAlreadyExistsException(theRole.getName()+ " role already exists");
         }
         return roleRepository.save(role);
