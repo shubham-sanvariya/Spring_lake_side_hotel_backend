56b9f1c6e03da3ea337a28b5458ebb5ffea847e0
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-17 10:47:36 +0530

    adding pre-authorize condition

--- src/main/java/com/hotel_project/lake_side_hotel/controller/UserController.java
+++ src/main/java/com/hotel_project/lake_side_hotel/controller/UserController.java
@@ -5,6 +5,7 @@ import com.hotel_project.lake_side_hotel.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +18,14 @@ public class UserController {
     private final IUserService userService;
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<User>> getAllUsers(){
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
     }
 
 
     @GetMapping("/{email}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
         try {
             User theUser = userService.getUser(email);
@@ -36,6 +39,7 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{userId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or (hasRole('ROLE_USER') and #email == principal.username)")
     public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
         try {
             userService.deleteUser(email);
